84418088a4d11c6689257375de8a6a8849e9a193
Bruno Dias <bruno.dias@vtex.com.br>
2019-05-07 23:47:00 -0300

    Dispatch orderPlaced event

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,10 +7,18 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- Dispatch `orderPlaced` event.
+
 ## [0.1.0] - 2019-05-06
-### Added 
-- Added listener for productDetils events.
+
+### Added
+
+- Added listener for `productDetails` event.
 
 ## [0.0.2] - 2019-05-01
+
 ### Added
+
 - Added listener for pageview events.
--- react/index.tsx
+++ react/index.tsx
@@ -1,11 +1,18 @@
 const gaId = window.__SETTINGS__.gaId
 
 if (!gaId) {
-  throw new Error('Warning: No Google Analytics ID is defined. To setup the app, go to your admin.')
+  throw new Error(
+    'Warning: No Google Analytics ID is defined. To setup the app, go to your admin.'
+  )
 }
 
 // Initialize async analytics
-window.ga = window.ga || function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+window.ga =
+  window.ga ||
+  function() {
+    ;(ga.q = ga.q || []).push(arguments)
+  }
+ga.l = +new Date()
 ga('create', `${gaId}`, 'auto')
 ga('require', 'ec')
 
@@ -18,13 +25,13 @@ document.head!.prepend(script)
 let currentUrl = ''
 
 // Common pageview function
-const pageView = (data:any) => {
+const pageView = (data: any) => {
   currentUrl = data.pageUrl
   ga('set', {
     page: currentUrl.replace(location.origin, ''),
-    ...((data.pageTitle) && {
-      title: data.pageTitle
-    })
+    ...(data.pageTitle && {
+      title: data.pageTitle,
+    }),
   })
   ga('send', 'pageview')
 }
@@ -32,18 +39,26 @@ const pageView = (data:any) => {
 // Common ProductDetail function
 const productDetail = (product: any) => {
   ga('ec:addProduct', {
-    'id': product.productId,
-    'name': product.productName,
-    'category': product.categoryId,
-    'brand': product.brand,
-    'variant': product.variant,
-  });
+    id: product.productId,
+    name: product.productName,
+    category: product.categoryId,
+    brand: product.brand,
+    variant: product.variant,
+  })
   ga('ec:setAction', 'detail')
 }
 
+// Common OrderPlaced function
+const orderPlaced = (order: any) => {
+  ga('send', 'event', {
+    eventCategory: 'orderPlaced',
+    eventAction: 'view',
+    ...order,
+  })
+}
 // Event listener for pageview
 window.addEventListener('message', e => {
-  if(e.data.pageUrl && e.data.pageUrl !== currentUrl) {
+  if (e.data.pageUrl && e.data.pageUrl !== currentUrl) {
     switch (e.data.event) {
       case 'pageView': {
         pageView(e.data)
@@ -62,5 +77,10 @@ window.addEventListener('message', e => {
   // Event listener for productDetail
   if (e.data.event === 'productView') {
     productDetail(e.data.product)
+  }
+
+  // Event listener for orderPlaced
+  if (e.data.event === 'orderPlaced') {
+    orderPlaced(e.data)
   }
 })
