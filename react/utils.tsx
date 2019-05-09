7155dae4a731108171f1f20881040c3da204847c
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2019-05-08 23:27:48 -0300

    Disptach productClick event to google-analytics

---
+++ react/utils.tsx
@@ -0,0 +1,14 @@
+export const getCategory = (rawCategories: any) => {
+  if (!rawCategories || !rawCategories.length) return
+
+  const _filterEmpty = (string: string) => {
+    if (string.length > 0) return string
+  }
+
+  const categories = rawCategories.map(function(categoryPath: string) {
+    let splitedPath = categoryPath.split('/').filter(_filterEmpty)
+    return splitedPath[0]
+  })
+
+  return categories ? categories[0] : categories
+}
